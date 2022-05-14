77f518dbe3e3abf48215659cdf612b129e7624e3
lulongfei <lulongfei172006@126.com>
2022-05-14 22:03:48 +0800

    update codes

--- mutexAndDataRacing.cpp
+++ mutexAndDataRacing.cpp
@@ -2,16 +2,16 @@
 #include <thread>
 #include <mutex>
 
-std::mutex mu;
 
 using namespace std;
+std::mutex mu;
 
 void shared_print(std::string message, int id)
 {
-    mu.lock();
+    // mu.lock();
     // std::unique_lock<std::mutex> guard(mu);
     cout << message << id << endl;
-    mu.unlock();
+    // mu.unlock();
 }
 
 void myFunc()
@@ -26,15 +26,35 @@ void myFunc()
 
 int main()
 {
-
-    thread t1(myFunc);
-    thread t2(myFunc);
+    std::thread t1(myFunc);
     for(int i=0; i < 10; i++)
     {
         shared_print("Call from main: ", i);
     }
 
     t1.join();
-    t2.join();
     return 0;
 }
+
+// std::mutex mtx;
+
+// void print_block(int n, char c)
+// {
+//     std::unique_lock<std::mutex> lck(mtx);
+//     for(int i = 0; i < n; i++)
+//     {
+//         std::cout << c;
+//     }
+//     std::cout << '\n';
+// }
+
+// int main()
+// {
+//     std::thread th1(print_block, 50, '*');
+//     std::thread th2(print_block, 50, '$');
+
+//     th2.join();
+//     th1.join();
+
+//     return 0;
+// }
